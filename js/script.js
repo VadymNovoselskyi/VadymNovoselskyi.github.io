b86e86cbbf70057faeaf566453e8bc60e04cfe6e
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2024-03-18 13:42:28 +0100

    Add some js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ let lcd = null; // displayen
 
 let memory = 0; // Lagrat/gamlat värdet från display
 let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
+let presentComma = false;
 
 function init() {
     lcd = document.getElementById('lcd');
@@ -21,13 +22,43 @@ function init() {
 function buttonClick(e) {
     let btn = e.target.id; //id för den tangent som tryckte ner
 
-
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
         let digit = btn.substring(1, 2); // plockar ut siffran från id:et
 
+        addDigit(digit);
     } else { // Inte en siffertangent, övriga tangenter.
-
+        switch (btn) {
+            case "add":
+                setOperator(btn);
+                memory = lcd.value;
+                clearLCD();
+                break;
+            case "sub":
+                setOperator(btn);
+                memory = lcd.value;
+                clearLCD();
+                break;
+            case "mul":
+                setOperator(btn);
+                memory = lcd.value;
+                clearLCD();
+                break;
+            case "div":
+                setOperator(btn);
+                memory = lcd.value;
+                clearLCD();
+                break;
+            case "comma":
+                addComma();
+                break;
+            case "enter":
+                calculate();
+                break;
+            case "clear":
+                memClear();
+                break            
+        }
     }
 }
 
@@ -35,13 +66,23 @@ function buttonClick(e) {
  *  Lägger till siffra på display.
  */
 function addDigit(digit) {
+    lcd.value += digit;
 }
 
 /**
  * Lägger till decimaltecken
  */
 function addComma() {
+    presentComma = false;
+    for (let i = 0; i < lcd.value.length; i++){
+        if (lcd.value.substring(i, i + 1) == ',') {
+            presentComma = true;
+        }
+    }  
 
+    if (!presentComma && lcd.value.length != 0) {
+        lcd.value += ',';
+    }  
 }
 
 /**
@@ -49,20 +90,32 @@ function addComma() {
  * +, -, *, /
  */
 function setOperator(operator){
-
+    arithmetic = operator;
 }
 
 /**
  * Beräknar ovh visar resultatet på displayen.
  */
 function calculate() {
-
+    switch (arithmetic) {
+        case "add":
+            lcd.value = parseFloat(memory.replace(",", ".")) + parseFloat(lcd.value.replace(",", "."));
+            break;
+        case "sub":
+            lcd.value = parseFloat(memory.replace(",", ".")) - parseFloat(lcd.value.replace(",", "."));
+            break;
+        case "mul":
+            lcd.value = parseFloat(memory.replace(",", ".")) * parseFloat(lcd.value.replace(",", "."));
+            break;
+        case "div":
+            lcd.value = parseFloat(memory.replace(",", ".")) / parseFloat(lcd.value.replace(",", "."));
+            break;
+    }
 }
 
 /** Rensar display */
 function clearLCD() {
     lcd.value = '';
-    isComma = false;
 }
 
 /** Rensar allt, reset */
